d2fc59e7373832752b7a45f57cf1ab2ade2dd811
Ateeq <atiqdon2404@gmail.com>
2026-05-06 11:10:44 +0500

    feat: initialize CV storage migration and refresh service integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:android_cv_maker/data/local/cv_storage.dart';
+import 'package:android_cv_maker/services/refresh_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'core/themes/theme_provider.dart';
@@ -6,6 +8,7 @@ import 'presentation/screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await CVStorage().migrateOldData();
   runApp(const MyApp());
 }
 
@@ -15,7 +18,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => ThemeProvider())],
+      providers: [
+        ChangeNotifierProvider(create: (_) => ThemeProvider()),
+        ChangeNotifierProvider(create: (_) => RefreshService()),
+      ],
       child: Consumer<ThemeProvider>(
         builder: (context, themeProvider, child) {
           return MaterialApp(
